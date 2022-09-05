bb049c2a4dfd9dce1c9111e949948e30a145307a
William Henderson <william-henderson@outlook.com>
2022-09-05 10:53:16 +0100

    Configured embed image script to only run in production

--- scripts/onPostBuild.bat
+++ scripts/onPostBuild.bat
@@ -1,4 +1,6 @@
 @echo off
 
+if %STUART_ENV%==development exit 0
+
 cd scripts/embed
 npm i && npm start
--- scripts/onPostBuild.sh
+++ scripts/onPostBuild.sh
@@ -1,2 +1,6 @@
+if [ "$STUART_ENV" == "development" ]; then
+  exit 0
+fi
+
 cd scripts/embed
 npm i && npm start
